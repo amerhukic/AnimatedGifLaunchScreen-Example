a6607405e0d3b74d706899b5500a7cf868ee3e71
Amer Hukic <hukicamer@gmail.com>
2018-09-13 09:18:00 +0200

    Initial commit

---
+++ AnimatedGifLaunchScreen-Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimatedGifLaunchScreen-Example
+//
+//  Created by Amer Hukic on 13/09/2018.
+//  Copyright © 2018 Amer Hukic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
